d15670b72d476d3f175c404b7c3730df39f7e862
zobry <apirat.chiwa@gmail.com>
2020-07-25 19:52:45 +0700

    add new link

--- basic/resources/views/home.blade.php
+++ basic/resources/views/home.blade.php
@@ -1,11 +1,14 @@
 @extends('layout')
 @section('title', 'Home page')
 @section('content')
-    <h1>Welcome to home page</h1>
-    <p>Comming soon..</p>
+<h1>Welcome to home page</h1>
+<p>Comming soon..</p>
+<nav>
+    <a class="btn btn-primary" href="{{ route('user.create') }}">New User</a>
+</nav>
 @stop
 @section('footer')
-    <h3>contact me</h3>
+<h3>contact me</h3>
 @stop
 
 <p>show other content</p>
