70782ac444cca6a8808e17edf0bdfb92c2a4298c
zobry <apirat.chiwa@gmail.com>
2020-07-26 20:24:49 +0700

    delete user

--- basic/resources/views/user/index.blade.php
+++ basic/resources/views/user/index.blade.php
@@ -5,13 +5,39 @@
     <div class="row">
         <div class="col-12">
             <a class="btn btn-primary" href="{{ route('user.create') }}">New User</a>
+            @if(\Session::has('success'))
+            <div class='alert alert-success'>
+                <p class="mb-0">{{ \Session::get('success') }}</p>
+            </div>
+            @endif
             <ul>
                 @foreach($users as $user)
-                <li>{{ $user['fname'] }} {{ $user['lname'] }} <a class="text-primary ml-3" href="#">Edit</a> <a
-                        class="text-danger ml-3" href="#">Del</a></li>
+                <li class="mb-3">{{ $user['fname'] }} {{ $user['lname'] }}
+                    <a class="text-primary ml-3" href="#">Edit</a>
+                    <form action="{{ action('UserController@destroy', $user['id']) }}" method="post"
+                        class="delete_form">
+                        {{ csrf_field() }}
+                        <input type="hidden" name="_method" value="DELETE" />
+                        <button class="btn btn-danger" type="submit">Delete</button>
+                    </form>
+                </li>
                 @endforeach
             </ul>
         </div>
     </div>
 </div>
+
+@section('script')
+<script type="text/javascript">
+$(document).ready(function() {
+    $('.delete_form').on('submit', function() {
+        if (confirm('คุณต้องการลบข้อมูลหรือไม่ ?')) {
+            return true;
+        } else {
+            return false;
+        };
+    });
+});
+</script>
+@endsection
 @endsection
